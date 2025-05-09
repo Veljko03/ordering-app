e1d3fc96c1877572ccf40a21a6b82b0820c9f000
Veljko03 <veljkodovragovic@gmail.com>
2025-05-09 21:26:11 +0200

    added schema for catefory and item

---
+++ app/models/Item.js
@@ -0,0 +1,27 @@
+const { Schema, model, models } = require("mongoose");
+
+const ItemSchema = new Schema({
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  subcategoryName: String,
+  name: String,
+  description: String,
+  imageUrl: String,
+  sizes: [SizesSchema],
+  addons: [AddonsSchema],
+});
+
+const SizesSchema = new Schema({
+  size: String,
+  price: Number,
+});
+
+const AddonsSchema = new Schema({
+  name: String,
+  price: Number,
+  active: Boolean,
+});
+
+export default models?.Item || model("Item", ItemSchema);
