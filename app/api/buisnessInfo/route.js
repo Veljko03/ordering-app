ca76ff99083adf23052fecae801c25c532c6da0d
Veljko03 <veljkodovragovic@gmail.com>
2025-05-10 12:10:18 +0200

    created get and post requests for handling buisness info

---
+++ app/api/buisnessInfo/route.js
@@ -0,0 +1,37 @@
+import GeneralInfo from "@/app/models/GeneralInfo";
+import mongoose from "mongoose";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+
+    console.log("this is body ", body);
+
+    await mongoose.connect(process.env.NEXT_MONGO_URL);
+
+    const existing = await GeneralInfo.findOne();
+
+    let result;
+
+    if (existing) {
+      // ako postoji azurira se, ako ne postoji pravi se novi
+      await GeneralInfo.updateOne({ _id: existing._id }, body);
+      result = await GeneralInfo.findById(existing._id);
+    } else {
+      result = await GeneralInfo.create(body);
+    }
+
+    return new Response(JSON.stringify(result), { status: 201 });
+  } catch (error) {
+    console.error("Error saving schedule:", error);
+    return new Response(JSON.stringify({ error: "Failed to save schedule" }), {
+      status: 500,
+    });
+  }
+}
+
+export async function GET() {
+  await mongoose.connect(process.env.NEXT_MONGO_URL);
+  const schedules = await GeneralInfo.find();
+  return new Response(JSON.stringify(schedules), { status: 201 });
+}
