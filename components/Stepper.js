323522a4458f44a503bb5d34d62708c31af05717
Veljko03 <veljkodovragovic@gmail.com>
2025-06-23 15:29:02 +0200

    styling stepper

--- components/Stepper.js
+++ components/Stepper.js
@@ -1,22 +1,31 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import {
   FaRocket,
   FaTools,
   FaPen,
   FaDollarSign,
   FaClipboardList,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
-
+import { HiBuildingStorefront } from "react-icons/hi2";
+import { FaCalendarDays } from "react-icons/fa6";
+import { MdMenuBook } from "react-icons/md";
+import { MdRestaurantMenu } from "react-icons/md";
+import { BiSolidCategoryAlt } from "react-icons/bi";
 
 const steps = [
-  { id: 1, name: 'Info', icon: <FaRocket />, path: '/buisnessInfo' },
-  { id: 2, name: 'Week', icon: <FaTools />, path: '/schedule' },
-  { id: 3, name: 'Category', icon: <FaPen />, path: '/category' },
-  { id: 4, name: 'Items', icon: <FaDollarSign />, path: '/item' },
-  { id: 5, name: 'Review', icon: <FaClipboardList />, path: '/review' },
+  {
+    id: 1,
+    name: "",
+    icon: <HiBuildingStorefront />,
+    path: "/buisnessInfo",
+  },
+  { id: 2, name: "", icon: <FaCalendarDays />, path: "/schedule" },
+  { id: 3, name: "", icon: <MdMenuBook />, path: "/category" },
+  { id: 4, name: "", icon: <MdRestaurantMenu />, path: "/item" },
+  { id: 5, name: "", icon: <BiSolidCategoryAlt />, path: "/review" },
 ];
 
 const Stepper = () => {
@@ -33,23 +42,27 @@ const Stepper = () => {
   const handleNext = () => {
     const nextIndex = Math.min(currentStep + 1, steps.length - 1);
     setCurrentStep(nextIndex);
-     router.push(steps[nextIndex].path);
+    router.push(steps[nextIndex].path);
 
-   // window.location.href = steps[nextIndex].path;
+    // window.location.href = steps[nextIndex].path;
   };
 
   return (
-    <div className="flex items-center justify-between px-4 py-6 bg-[#101010] border border-gray-700 rounded-lg">
+    <div className="flex items-center justify-center gap-4 sm:gap-8 md:gap-12 lg:gap-18 px-4 py-6 bg-[#101010] border border-gray-700 rounded-lg">
       {steps.map((step, index) => (
-        <div key={step.id} className="flex items-center relative cursor-pointer" onClick={() => handleStepClick(index)}>
+        <div
+          key={step.id}
+          className="flex items-center relative cursor-pointer"
+          onClick={() => handleStepClick(index)}
+        >
           {index > 0 && (
-            <div className="absolute -left-6 top-1/2 w-6 h-0.5 bg-gray-600" />
+            <div className="absolute -left-14 top-1/2 w-14 h-0.5 bg-gray-600" />
           )}
           <div
-            className={`w-18 h-18 flex flex-col items-center justify-center rounded-full border-2 text-sm font-medium ${
+            className={`w-16 h-16 flex flex-col items-center justify-center rounded-full border-2 text-sm font-medium ${
               index === currentStep
-                ? 'bg-custom-blue text-white border-custom-blue'
-                : 'bg-black border-gray-500 text-gray-300'
+                ? "bg-[#7893c3] text-white border-[#7893c3]"
+                : "bg-black border-gray-500 text-gray-300"
             }`}
           >
             <div className="text-lg">{step.icon}</div>
@@ -63,7 +76,7 @@ const Stepper = () => {
 
       <button
         onClick={handleNext}
-        className="ml-6 px-4 py-2 bg-custom-blue text-white rounded-lg cursor-pointer "
+        className="ml-6   bg-custom-blue text-white rounded-lg cursor-pointer "
         disabled={currentStep === steps.length - 1}
       >
         Next
