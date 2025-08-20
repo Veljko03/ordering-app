272a0364243e84b44946296cc01ee2fdf7219182
veljko03 <veljkodovragovic@gmail.com>
2025-08-21 01:21:04 +0200

    created page for cart

---
+++ app/cart/page.js
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Cart() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+      <h1 className="text-black">Ovde ce biti prikazani itemi</h1>
+    </div>
+  );
+}
--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { FaShoppingCart, FaLocationArrow, FaClock } from "react-icons/fa";
 import Places from "./PlacePicker";
+import { useRouter } from "next/navigation";
 // import { Button } from "@/components/ui/button";
 // import { Badge } from "@/components/ui/badge";
 // import { useCart } from "@/context/CartContext";
@@ -10,11 +11,15 @@ import Places from "./PlacePicker";
 const Header = () => {
   //onst { toggleCart, itemCount } = useCart();
   const [itemCount, setItemCount] = useState(1);
+  const router = useRouter();
   return (
     <header className="sticky top-0 z-50 bg-background/95 backdrop-blur 0 border-b-1 border-orange-400">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         {/* Logo dodati mozda */}
-        <div className="flex items-center space-x-2">
+        <div
+          className="flex items-center space-x-2 cursor-pointer"
+          onClick={() => router.push("/")}
+        >
           <div className="w-8 h-8 bg-orange-400 rounded-full flex items-center justify-center">
             <span className="text-black font-bold text-sm">R</span>
           </div>
@@ -38,6 +43,7 @@ const Header = () => {
           size="sm"
           // onClick={toggleCart}
           className="relative transition-smooth hover:scale-105 cursor-pointer "
+          onClick={() => router.push("/cart")}
         >
           <FaShoppingCart className="w-6 h-6  mr-2 text-black" />
           {itemCount > 0 && (
